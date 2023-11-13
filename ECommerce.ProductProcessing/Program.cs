fc6a7edfca66ed06569ca86e5af0ab445cc17c56
desireeacosta <desiree.acosta.60086@usalesiana.edu.bo>
2023-11-12 20:40:56 -0400

    Added ecommerce sln file and ProductProcessing console

---
+++ ECommerce.ProductProcessing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+Console.WriteLine("Processing started... :)");
+
+var factory = new ConnectionFactory()
+{
+  HostName = "jackal.rmq.cloudamqp.com",
+  UserName = "zebbopeh",
+  Password = "CW6QcuLaGs22SGVtnPvsQToJwDnpPcli",
+  VirtualHost = "zebbopeh"
+};
+
+var conn = factory.CreateConnection();
+using var channel = conn.CreateModel();
+channel.QueueDeclare("products", durable: true, exclusive: false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, eventArgs) =>
+{
+  var body = eventArgs.Body.ToArray();
+  var message = Encoding.UTF8.GetString(body);
+  Console.WriteLine($"A message has been received: {message}");
+};
+
+channel.BasicConsume("products", true, consumer);
+
+Console.ReadKey();
